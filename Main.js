87086c009fe935a969f5bfbbd3a89785ea1bd3db
scats <65074620+ScatskingZ@users.noreply.github.com>
2021-10-21 15:14:28 +0200

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,24 @@
+//initiate all variables
+var tools = document.getElementById("tools");
+var logo = document.getElementById("logo");
+var ScreenHeight, ScreenWidth, ToolbarHeight;
+
+//consistently update the website's positions according to the screen size
+setInterval(function(){
+    //get the screen's size
+    ScreenHeight = window.innerHeight;
+    ScreenWidth = window.innerWidth;
+
+    //set the toolbar height
+    ToolbarHeight = ScreenHeight / 5;
+
+    //update the upper tools' size and position
+    tools.style.width = ScreenWidth + 'px';
+    tools.style.height = ToolbarHeight + 'px';
+
+    //update the logo's size and position
+    logo.style.height = ToolbarHeight + 'px';
+    logo.style.width = ToolbarHeight + 'px';
+    logo.style.left = ScreenWidth / 50 + 'px';
+
+}, 200);
