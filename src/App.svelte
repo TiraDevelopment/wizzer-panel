1b06fa0d9d99a27aa2ec46d71babbb386794de3c
moni <aanilsaint26@gmail.com>
2021-12-05 12:00:55 +0000

    Add OG Tags

--- src/App.svelte
+++ src/App.svelte
@@ -38,6 +38,23 @@
 <svelte:head>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+	<!-- Primary Meta Tags -->
+<title>Wizzer Panel — Servers</title>
+<meta name="title" content="Wizzer Panel — Servers">
+<meta name="description" content="Create your own server on Wizzer Today!">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://wizzer-panel.netlify.app/">
+<meta property="og:title" content="Wizzer Panel — Servers">
+<meta property="og:description" content="Create your own server on Wizzer Today!">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://wizzer-panel.netlify.app/">
+<meta property="twitter:title" content="Wizzer Panel — Servers">
+<meta property="twitter:description" content="Create your own server on Wizzer Today!">
 </svelte:head>
 
 <main>
